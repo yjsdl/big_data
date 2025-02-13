cc8a1431f691a5341d7ad2b8087334a629239ad5
workwindows <yj160521@163.com>
2025-02-13 17:35:12 +0800

    .

--- data_analysis_server/__init__.py
+++ data_analysis_server/__init__.py
@@ -2,8 +2,4 @@
 # @date：2025/1/8 11:08
 # @Author：LiuYiJie
 # @file： __init__.py
-import pyarrow
-import pandas as pd
 
-print(f"PyArrow version: {pyarrow.__version__}")
-print(f"Pandas version: {pd.__version__}")
