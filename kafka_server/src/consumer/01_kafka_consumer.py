18a019152f558914f3d61fceaa557a446dbcdebe
workwindows <yj160521@163.com>
2025-02-25 21:29:35 +0800

    kafka消费者

---
+++ kafka_server/src/consumer/01_kafka_consumer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @date：2025/2/19 16:21
+# @Author：LiuYiJie
+# @file： 01_kafka_consumer
+from kafka import KafkaConsumer
+
+
+def kafka_consumer_test():
+    # 创建kafkaConsumer
+    consumer = KafkaConsumer(
+        'testTopic', 'testTopic01',  # 同时消费多个topic
+        bootstrap_servers=['hadoop01:9092', 'hadoop02:9092', 'hadoop03:9092'],
+        group_id='group_1'
+    )
+
+    # 获取消费数据
+    for message in consumer:
+        topic = message.topic
+        partition = message.partition
+        offset = message.offset
+        key = message.key
+        value = message.value.decode('utf-8')
+        print(f'从 {topic} 的 {partition} 分区上, 获取到第 {offset} 偏移量的消息为 {key}-{value}')
+
+
+if __name__ == '__main__':
+    kafka_consumer_test()
