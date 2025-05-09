1ec6f1056492e69e63d52515bcb1f47b42b8b9d0
workwindows <yj160521@163.com>
2025-05-09 11:20:44 +0800

    日志代码优化

--- module_server/setting.py
+++ module_server/setting.py
@@ -15,10 +15,13 @@ LOG_LEVEL_FILE = 'INFO'  # 日志输出到文件的等级
 LOG_FILE_SIZE = 10 * 1024 * 1024  # 每个日志文件大小
 LOG_BACKUP_COUNT = 10  # 保留日志文件数
 LOG_FILTER_ENABLED = True  # 是否启动日志过滤
+LOG_SPLIT_ERROR = False  # 设置为 True 则开启错误日志分离
+LOG_JSON_OUTPUT = False  # 设置为 True 则支持 JSON 格式输出，日志文件以json显示
 LOG_FILTER_PYPI = [
     'pymysql',
     'pymongo',
-    'redis'
+    'redis',
+    'kafka'
 ]
 # 要过滤掉日志中包含这些关键词的日志项
 LOG_FILTER_KEYWORDS = [
